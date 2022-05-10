209c3006a8c35e9be41456261bbb87da5735a5e9
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2022-05-09 23:52:37 -0400

    assignment repo for ecommerce

---
+++ Homework/routes/Products.js
@@ -0,0 +1,17 @@
+const app = require("express");
+const router = express.Router();
+
+const productCtrl = require("../controllers/Products");
+
+router.get("/", productCtrl.index);
+router.get("/new", productCtrl.form);
+router.get("/:id", productCtrl.show);
+router.get("/:id/edit", productCtrl.form);
+
+router.post("/", productCtrl.create);
+router.post("/:id", productCtrl.update);
+router.post("/:id/delete", productCtrl.remove);
+
+router.delete("/:id", productCtrl.remove);
+
+module.exports = router;
