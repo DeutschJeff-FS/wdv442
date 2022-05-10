a4950878190116e2c877182abd5267fee418b9c2
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2022-05-09 20:59:54 -0400

    complete route

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ app.get("/", (req, res) => {
   res.send(`Assignment page!`);
 });
 
-// How does "all" equal to a set of given queries?
 app.get("/products/all", (req, res) => {
-  res.header("");
-  res.send(`Products page!`);
-});
-
-app.get("/products/:id", (req, res) => {
-  res.send(`Product, ${req.params.id}`);
+  JSON.stringify(req.query) === "{}"
+    ? res.send(`Products, ${req.headers.page}, ${req.headers.sort}, ${req.headers.order}`)
+    : res.send(`Products, ${req.query.sort}`);
 });
 
 app.get("/products/:id-:size-:color", (req, res) => {
   res.send(`Product, ${req.params.id}, ${req.params.size}, ${req.params.color}`);
+});
+
+app.get("/products/:id", (req, res) => {
+  res.send(`Product, ${req.params.id}`);
 });
 
 app.listen(3000);
